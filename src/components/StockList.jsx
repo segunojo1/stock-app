00ce2430f5708a164a099a81fb2717c3095dd7e1
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-09 20:45:34 +0100

    display data

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -5,9 +5,17 @@ const StockList = () => {
     const [stocks, setstocks] = useState()
     const [watchedStocks, setWatchedStocks] = useState(['GOOGL', 'MSFT', 'AMZN'])
 
+    const getColor = (change) => {
+         if(change > 0) {
+          return 'green'
+         }else{
+          return 'red'
+         }
+    }
     useEffect(() => {
       let isMounted = true;
       const responses = []
+
         const fetchFinn = async () => {
             try {
                const respo = await Promise.all(
@@ -39,7 +47,34 @@ const StockList = () => {
     }, [])
   return (
     <div>
-        
+       <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Last</th>
+            <th>Chg</th>
+            <th>Chg%</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Open</th>
+            <th>Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stocks.map((stock) => {
+            return <tr>
+              <td>{stock.symbol}</td>
+              <td>{stock.data.c}</td>
+              <td className={`text-[${getColor(stock.data)}]`}>{stock.symbol}</td>
+              <td>{stock.symbol}</td>
+              <td>{stock.symbol}</td>
+              <td>{stock.symbol}</td>
+              <td>{stock.symbol}</td>
+
+            </tr>
+          })}
+        </tbody>
+        </table> 
     </div>
   )
 }
