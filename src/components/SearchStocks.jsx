5a7af10bf2b7b09af96a3ebc7cd6d609249965eb
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-11 00:27:54 +0100

    search

--- src/components/SearchStocks.jsx
+++ src/components/SearchStocks.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SearchStocks = () => {
   return (
-    <div>SearchStocks</div>
+    <div className='w-fit mx-auto'>
+      <input type="text" placeholder='search' className='p-[1rem] border mx-auto'/>
+    </div>
   )
 }
 
