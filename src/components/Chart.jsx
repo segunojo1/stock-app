2a587328311de7f445b966970013c0e0e25a66f6
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-13 00:23:34 +0100

    feature: chart data ready

---
+++ src/components/Chart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Chart = ({chartData}) => {
+    const {day, week, year} = chartData
+  return (
+    <div>Chart</div>
+  )
+}
+
+export default Chart
--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import finnhub from '../apis/finnhub'
+import Chart from '../components/chart'
 
+
+const formatData = (data) => {
+      return data.t.map((elem, index) => {
+         return {
+          x: elem * 1000,
+          y: data.c[index]
+         }
+     })
+}
 const StockDetail = () => {
   const {symbol} = useParams()
+  const [chartData, setChartData] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,24 +24,69 @@ const StockDetail = () => {
         oneDayAgo = currentTime - 3600* 24 * 2
       }else if(new Date().getDay() == 0){
         oneDayAgo = currentTime - 3600* 24 * 3
+      }else{
+        oneDayAgo = currentTime - 3600 * 24
       }
+      const oneWeekAgo = currentTime - 3600* 24 * 7
+      const oneYearAgo = currentTime - 3600* 24 * 365
       console.log(new Date().getDay());
-         const response = await finnhub.get('/stock/candle', {
-          params: {
-            symbol,
-            to: currentTime,
-            from: oneDayAgo,
-            resolution: 30
+      try {
+        const res = await Promise.all([finnhub.get('/stock/candle', {
+           params: {
+             symbol,
+             to: currentTime,
+             from: oneDayAgo,
+             resolution: 30
+           }
+          }),
+          finnhub.get('/stock/candle', {
+           params: {
+             symbol,
+             to: currentTime,
+             from: oneWeekAgo,
+             resolution: 60
+           }
+          }),
+          finnhub.get('/stock/candle', {
+           params: {
+             symbol,
+             to: currentTime,
+             from: oneYearAgo,
+             resolution: 'W'
+           }
+          })
+        ])
+        setChartData({
+            day: formatData(res[0].data),
+            week : formatData(res[1].data),
+            year: formatData(res[2].data)
           }
-         })
-         console.log(response);
+        )
+
+        console.log(res);
+        
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchData()
   }, [])
 
   return (
-    <div>StockDetail {symbol}</div>
+    <div>StockDetail {symbol}
+    {chartData && <Chart chartData={chartData} symbol={symbol}/>}
+    </div>
   )
 }
+// const chartData = [{
+//   day: formatData(),
+//   week : formatData(),
+//   year: formatData()
+// }]
 
+// const data = [
+//   {x: t,
+//    y: c
+//   }
+// ]
 export default StockDetail
