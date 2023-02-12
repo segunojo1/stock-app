fa6e051580274e49297d09351b9e8ad13f197715
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-12 19:59:58 +0100

    feature: stock detail page

--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import finnhub from '../apis/finnhub'
 
 const StockDetail = () => {
+  const {symbol} = useParams()
+
+  useEffect(() => {
+    const fetchData = async() => {
+         const response = finnhub.get('/stock/candle', {
+          params: {
+            symbol,
+            to,
+            from,
+            resolution
+          }
+         })
+    }
+  })
+
   return (
-    <div>StockDetail</div>
+    <div>StockDetail {symbol}</div>
   )
 }
 
