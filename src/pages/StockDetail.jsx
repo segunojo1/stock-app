4b8bcae1daa3f4767c6d929dd2c368aecf56ef6e
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-12 20:17:14 +0100

    getdata

--- src/pages/StockDetail.jsx
+++ src/pages/StockDetail.jsx
@@ -6,17 +6,27 @@ const StockDetail = () => {
   const {symbol} = useParams()
 
   useEffect(() => {
-    const fetchData = async() => {
-         const response = finnhub.get('/stock/candle', {
+    const fetchData = async () => {
+      const currentTime = Math.floor(new Date().getTime() / 1000)
+      let oneDayAgo;
+      if(new Date().getDay() == 6) {
+        oneDayAgo = currentTime - 3600* 24 * 2
+      }else if(new Date().getDay() == 0){
+        oneDayAgo = currentTime - 3600* 24 * 3
+      }
+      console.log(new Date().getDay());
+         const response = await finnhub.get('/stock/candle', {
           params: {
             symbol,
-            to,
-            from,
-            resolution
+            to: currentTime,
+            from: oneDayAgo,
+            resolution: 30
           }
          })
+         console.log(response);
     }
-  })
+    fetchData()
+  }, [])
 
   return (
     <div>StockDetail {symbol}</div>
