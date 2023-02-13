b801eb8711cc129300aa2a0ce2ae2a1d8fe2bd9c
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-13 17:21:43 +0100

    feature: display chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 const Chart = ({chartData}) => {
-    const {day, week, year} = chartData
+    const {day, week, year} = chartData;
+    const options = {
+      title: {
+        text: symbol,
+        align: 'center',
+         style: {
+          fontSize: '24px'
+         }
+      },
+      chart: {
+        id: 'stock data',
+        animations: {
+          speed: 1300
+        }
+      },
+      xaxis: {
+         type: 'datetime',
+         labels: {
+          datetimeUTC: false
+         }
+      },
+      tooltip: {
+        
+      }
+    }
+
+    const series = [{
+      name: symbol,
+      data: day
+    }]
   return (
-    <div>Chart</div>
+    <div>
+      <Chart options={options} series={series} type='area' width="100%"/>
+    </div>
   )
 }
 
