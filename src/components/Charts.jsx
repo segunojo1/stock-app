a2a1e518517735eb79492b02d5828f53429b8dd5
lil-dev16 <olusegunkd2017@gmail.com>
2023-02-15 22:47:07 +0100

    featue: display based o selected dateformat

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import Chart from "react-apexcharts";
 
 const Charts = ({chartData, symbol}) => {
     const {day, week, year} = chartData;
+    const [dateFormat, setDateFormat] = useState('24h')
+
+    const getDateFormat  = () => {
+        switch (dateFormat) {
+          case '24h':
+            return day
+          case '7d':
+            return week
+          case '1yr':
+            return year
+          default:
+            break;
+        }
+    }
+    const getBtnColor = (val) => {
+      let classu ='p-4 text-black rounded-lg mr-[6px]';
+        if(val == dateFormat) {
+          return classu + ' bg-[aqua]'
+        }else{
+          return classu + ' bg-[white]'
+        }
+    }
     const options = {
       title: {
         text: symbol,
@@ -32,11 +55,16 @@ const Charts = ({chartData, symbol}) => {
 
     const series = [{
       name: symbol,
-      data: day
+      data: getDateFormat()
     }]
   return (
     <div className='p-4 mt-5'>
       <Chart options={options} series={series} type='area' width="100%" height='500' />
+      <div>
+        <button onClick={() => setDateFormat('24h')} className={getBtnColor('24h')}>Day</button>
+        <button onClick={() => setDateFormat('7d')} className={getBtnColor('7d')}>Week</button>
+        <button onClick={() => setDateFormat('1yr')} className={getBtnColor('1yr')}>Year</button>
+      </div>
     </div>
   )
 }
